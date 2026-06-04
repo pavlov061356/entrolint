62d0c137e81765086411d4a60488e739c2a3d428
Pavlov Vladimir <pavlovvova923@gmail.com>
2026-06-04 12:11:19 +0300

    feat(scaling): identifier_fanout detector via TypesInfo.Uses

--- cmd/entrolint/main.go
+++ cmd/entrolint/main.go
@@ -5,6 +5,7 @@ import (
 
 	// Blank-import detectors so their init() side-effect registers
 	// them in scaling.Registry before the CLI runs.
+	_ "github.com/pavlov061356/entrolint/internal/scaling/detectors/identifierfanout"
 	_ "github.com/pavlov061356/entrolint/internal/scaling/detectors/implementorscan"
 	_ "github.com/pavlov061356/entrolint/internal/scaling/detectors/shotgun"
 	_ "github.com/pavlov061356/entrolint/internal/scaling/detectors/switchsymmetry"
